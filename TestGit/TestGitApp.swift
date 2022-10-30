88edcbedcc743767e2fbf8855f6d6e1ac69934b8
RaysonYT <nikolomoec@gmail.com>
2022-10-30 22:42:57 +0200

    Initial Commit

---
+++ TestGit/TestGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitApp.swift
+//  TestGit
+//
+//  Created by Nikita Kolomoec on 30.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct TestGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
